657e6fcc80097a22ca2dca7132ab631d77d4c826
Giusseppe-97 <josepablonavarro97@gmail.com>
2021-09-04 23:41:54 -0500

    Create Codewars_series_sum_11.py
    write a function which returns the sum of following series up to nth term(parameter).
    Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
    You need to round the answer to 2 decimal places and return it as String.
    If the given value is 0 then it should return 0.00
    You will only be given Natural Numbers as arguments.

---
+++ Codewars_series_sum_11.py
@@ -0,0 +1,12 @@
+# write a function which returns the sum of following series upto nth term(parameter).
+# Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+# You need to round the answer to 2 decimal places and return it as String.
+
+# If the given value is 0 then it should return 0.00
+
+# You will only be given Natural Numbers as arguments.
+
+def series_sum(n):
+    return "{:.2f}".format(sum([(1/(3*i+1)) for i in range(0,n)]))
+
+print(series_sum(0))
