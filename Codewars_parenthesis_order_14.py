33883b0938f8fd13be151e72cfb4e6d580caefe9
Giusseppe-97 <josepablonavarro97@gmail.com>
2021-09-13 00:09:48 -0500

    Update Codewars_parenthesis_order_14.py

--- Codewars_parenthesis_order_14.py
+++ Codewars_parenthesis_order_14.py
@@ -11,48 +11,33 @@
 
 def valid_parentheses(string):
     pare = ""
-    cont_1=0
-    if string=="":
-        return False
+    cont_1 = 0
+    
     for i in range(len(string)):
         if string[i]=="(" or string[i]==")":
             pare = pare + string[i]
-    for j in range(len(pare)):
-        if pare[j]=="(":
-            cont_1 += 1
-        else:
-            cont_1 -= 1
-        if cont_1 == -1 or cont_1%2!=0:
+    if pare.count("(") == pare.count(")"):
+        for j in range(len(pare)):
+            if pare[j]=="(":
+                cont_1 += 1
+            if pare[j]==")":
+                cont_1 -= 1
+            if cont_1 == -1:
+                return False
+        if cont_1!=0 and cont_1%2!=0:
             return False
-            
+    else:
+        return False
+         
     return True
 
-# def valid_parentheses(string):
-#     i=0
-#     while i != (len(string)):
-
-#         if string[i] == "":
-#             return True
-
-#         elif string[i] == ")":
-#             break
-#         elif string[i]!= "(":
-#             i += 1
-#         else:
-#             open_par = string.count("(")
-#             close_par = string.count(")")
-#             if open_par == close_par:
-#                 return True
-#     return False
-
-print(valid_parentheses("  ("))
-print(1%2)
-
-# test.assert_equals(valid_parentheses("  ("),False)
-# test.assert_equals(valid_parentheses(")test"),False)
-# test.assert_equals(valid_parentheses(""),True)
-# test.assert_equals(valid_parentheses("hi())("),False)
-# test.assert_equals(valid_parentheses("hi(hi)()"),True)
+def valid_parentheses_best(string):
+    cnt = 0
+    for char in string:
+        if char == '(': cnt += 1
+        if char == ')': cnt -= 1
+        if cnt < 0: return False
+    return True if cnt == 0 else False
 
 
 
