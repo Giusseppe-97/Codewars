1eb7aa5aac18ffa71ae67bf08f29784ebe121f77
Giusseppe-97 <josepablonavarro97@gmail.com>
2021-09-12 21:08:20 -0500

    Update Codewars_parenthesis_order_14.py

--- Codewars_parenthesis_order_14.py
+++ Codewars_parenthesis_order_14.py
@@ -2,5 +2,31 @@
 # and determines if the order of the parentheses is valid. 
 # The function should return true if the string is valid, and false if it's invalid.
 
+"""
+    "()"              =>  true
+    ")(()))"          =>  false
+    "("               =>  false
+    "(())((()())())"  =>  true
+"""
+
 def valid_parentheses(string):
-    #your code here
+    i=0
+    while i != (len(string)):
+        if string[i] == ")":
+            break
+        elif string[i]!= "(":
+            i += 1
+        else:
+            open_par = string.count("(")
+            close_par = string.count(")")
+            if open_par == close_par:
+                return True
+    return False
+
+print(valid_parentheses(")()"))
+
+# test.assert_equals(valid_parentheses("  ("),False)
+# test.assert_equals(valid_parentheses(")test"),False)
+# test.assert_equals(valid_parentheses(""),True)
+# test.assert_equals(valid_parentheses("hi())("),False)
+# test.assert_equals(valid_parentheses("hi(hi)()"),True)
