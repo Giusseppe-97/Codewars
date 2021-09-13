145a331a31b07b254712ea73e114d0808b6d6a1e
Giusseppe-97 <josepablonavarro97@gmail.com>
2021-09-13 00:09:22 -0500

    Update Codewars_parenthesis_order_14.py

--- Codewars_parenthesis_order_14.py
+++ Codewars_parenthesis_order_14.py
@@ -10,27 +10,49 @@
 """
 
 def valid_parentheses(string):
-    i=0
-    while i != (len(string)):
-
-        if string[i] == "":
-            return True
-
-        elif string[i] == ")":
-            break
-        elif string[i]!= "(":
-            i += 1
+    pare = ""
+    cont_1=0
+    if string=="":
+        return False
+    for i in range(len(string)):
+        if string[i]=="(" or string[i]==")":
+            pare = pare + string[i]
+    for j in range(len(pare)):
+        if pare[j]=="(":
+            cont_1 += 1
         else:
-            open_par = string.count("(")
-            close_par = string.count(")")
-            if open_par == close_par:
-                return True
-    return False
+            cont_1 -= 1
+        if cont_1 == -1 or cont_1%2!=0:
+            return False
+            
+    return True
 
-print(valid_parentheses("x()(j(y())))uy(()quc"))
+# def valid_parentheses(string):
+#     i=0
+#     while i != (len(string)):
+
+#         if string[i] == "":
+#             return True
+
+#         elif string[i] == ")":
+#             break
+#         elif string[i]!= "(":
+#             i += 1
+#         else:
+#             open_par = string.count("(")
+#             close_par = string.count(")")
+#             if open_par == close_par:
+#                 return True
+#     return False
+
+print(valid_parentheses("  ("))
+print(1%2)
 
 # test.assert_equals(valid_parentheses("  ("),False)
 # test.assert_equals(valid_parentheses(")test"),False)
 # test.assert_equals(valid_parentheses(""),True)
 # test.assert_equals(valid_parentheses("hi())("),False)
 # test.assert_equals(valid_parentheses("hi(hi)()"),True)
+
+
+
