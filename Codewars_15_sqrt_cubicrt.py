0776515115897260c230da5e70b3ee3df5292dac
Giusseppe-97 <josepablonavarro97@gmail.com>
2021-09-15 23:11:10 -0500

    Update Codewars_15_sqrt_cubicrt.py

--- Codewars_15_sqrt_cubicrt.py
+++ Codewars_15_sqrt_cubicrt.py
@@ -1,11 +1,11 @@
-# def isPP(n):
-#     import math
-#     sr = math.sqrt(n)
-#     cr = n ** (1/3)
-#     if n%cr==0:
-#         return [int(cr),3]
-#     if n%sr==0:
-#         return [sr, 2] 
+def isPP(n):
+    import math
+    sr = math.sqrt(n)
+    cr = n ** (1/3)
+    if n%cr==0:
+        return [int(cr),3]
+    if n%sr==0:
+        return [sr, 2] 
 def isPP(n):
     for i in range(2,n):
         sr = round(n ** (1/i),ndigits=10)
