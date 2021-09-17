2c289353993fa6ad0eac5b1f7ecf9c4ca47287d1
Giusseppe-97 <josepablonavarro97@gmail.com>
2021-09-16 23:47:53 -0500

    Update Codewars_16_blind_numbers.py

--- Codewars_16_blind_numbers.py
+++ Codewars_16_blind_numbers.py
@@ -8,16 +8,16 @@ def blind_number(n):
         for j in range(n+1):
             z = '{num:02d}'.format(num=j)
             cont +=1
-            
+    for i in range(1,3):
+        for j in range(n):
+            r=a[j]+str(i)
+            lis.append(str(r))
+    
+
+    print(lis)       
 
     return(cont)
 
-    # for i in range(1,3):
-    #     for j in range(n):
-    #         r=a[j]
-    #         lis.append(str(r))
     
-
-    # print(lis)
 
 print(blind_number(3))
