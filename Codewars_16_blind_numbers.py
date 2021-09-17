81363a0e3137a5aafa83f0c0a28a4b4ef1b52ad0
Giusseppe-97 <josepablonavarro97@gmail.com>
2021-09-16 23:45:08 -0500

    Update Codewars_16_blind_numbers.py

--- Codewars_16_blind_numbers.py
+++ Codewars_16_blind_numbers.py
@@ -2,10 +2,15 @@ def blind_number(n):
     m = 3**(n)
     a = "0"*n
     lis = [a]
-    for i in range(n):
-        # print('{num:01d}'.format(num=i))
-        for j in range(n):
-            print('{num:02d}'.format(num=j))
+    cont=0
+    for i in range(n+1):
+        print('{num:01d}'.format(num=i))
+        for j in range(n+1):
+            z = '{num:02d}'.format(num=j)
+            cont +=1
+            
+
+    return(cont)
 
     # for i in range(1,3):
     #     for j in range(n):
