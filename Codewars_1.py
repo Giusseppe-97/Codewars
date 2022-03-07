d86df16fc9a3be90b1abeae09975c416473730cd
Giusseppe-97 <josepablonavarro97@gmail.com>
2022-03-07 18:02:53 -0500

    Update Codewars_1.py

--- Codewars_1.py
+++ Codewars_1.py
@@ -1,4 +1,7 @@
 def solution(string):
+    """
+    This function returns a word written backwords
+    """
     var = ""
     for i in range(len(string), 0, -1):
         var = var + string[i-1]
@@ -6,4 +9,35 @@ def solution(string):
         
     return var 
 
-print(solution("hola"))
+print(solution("paranguaricutrimicuaro"))
+
+
+def sumar(a, b): 
+    return a + b
+
+def restar(a, b): 
+    return a - b
+
+def multiplicar(a, b): 
+    return a*b
+
+def words_even_or_odd(string):
+    variable = len(string)
+    if variable%2 == 0:
+        return "Es par"
+    else: 
+        return "Es impar"
+
+print(words_even_or_odd("papaya"))
+
+print(multiplicar(8,22))
+print(restar(235635738,756583579))
+print(sumar(2356345738,33464756583579))
+
+import random
+
+lista = ["a", "b", "c", "d"]
+def aleatorio(lista):
+    rd = random.randint(0,3)
+    return lista[rd]
+print(aleatorio(lista))
