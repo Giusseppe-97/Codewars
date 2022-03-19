337055df7a70b14ec2449a0715449eecb435d69a
Giusseppe-97 <josepablonavarro97@gmail.com>
2022-03-19 12:01:16 -0500

    Update pandas_basics.py

--- pandas_fxns/pandas_basics.py
+++ pandas_fxns/pandas_basics.py
@@ -1,42 +1,53 @@
 import pandas as pd
+import numpy as np
 
-# read_csv()
-data_1 = pd.read_csv(r'C:UsersABCDesktopblog_dataset.csv')
-data_2 = pd.read_csv(r'C:UsersABCDesktopblog_dataset.csv')
+# # read_csv()
+# data_1 = pd.read_csv(r'C:UsersABCDesktopblog_dataset.csv')
+# data_2 = pd.read_csv(r'C:UsersABCDesktopblog_dataset.csv')
 
-# head()
-data_1.head(6)
+# # head()
+# data_1.head(6)
 
-# describe()
-data_1.describe()
+# # describe()
+# data_1.describe()
 
-# memory_usage()
-data_1.memory_usage(deep=True)
+# # memory_usage()
+# data_1.memory_usage(deep=True)
 
-# astype()
-data_1['Gender'] = data_1.Gender.astype('category')
+# # astype()
+# data_1['Gender'] = data_1.Gender.astype('category')
 
-# loc[:]
-data_1.loc[0:4, ['Name', 'Age', 'State']]
+# # loc[:]
+# data_1.loc[0:4, ['Name', 'Age', 'State']]
 
-# to_datetime()
-data_1['DOB'] = pd.to_datetime(data_1['DOB'])
+# # to_datetime()
+# data_1['DOB'] = pd.to_datetime(data_1['DOB'])
 
-# value_counts()
-data_1['State'].value_counts()
+# # value_counts()
+# data_1['State'].value_counts()
 
-# drop_duplicates()
-data_1.drop_duplicates(inplace=True)
+# # drop_duplicates()
+# data_1.drop_duplicates(inplace=True)
 
-# groupby()
-data_1.groupby(by='State').Salary.mean()
+# # groupby()
+# data_1.groupby(by='State').Salary.mean()
 
-# merge()
-data_1.merge(data_2, on='Name', how='left')
+# # merge()
+# data_1.merge(data_2, on='Name', how='left')
 
-# sort_values()
-data_1.sort_values(by='Name', inplace=True)
+# # sort_values()
+# data_1.sort_values(by='Name', inplace=True)
 
-# fillna()
-data_1['City temp'].fillna(38.5, inplace=True)
+# # fillna()
+# data_1['City temp'].fillna(38.5, inplace=True)
 
+# line = "shsodo soghsetion "
+# sum(map(line.split()))
+A = np.array([[0.1, 0.2], [0.03, 0.44], [0.25, 0.16]])
+
+print(900*1.25)
+print(750*1.75)
+print(700*2.25)
+print(600*2.75)
+print(450*3.25)
+print(400*3.75)
